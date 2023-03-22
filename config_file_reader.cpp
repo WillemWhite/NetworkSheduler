abec3df0aa367c7622b0ee87dfa41cfd63e6f909
Willem White <willemwhite128@gmail.com>
2023-03-22 11:47:27 +0300

    Correct some mistakes

--- config_file_reader.cpp
+++
@@ -1,6 +0,0 @@
-#include <fstream>
-
-bool ReadConfigFile()
-{
-
-}
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     if (argc != 2) {
         std::cout << "Usage: " << argv[0] << " <config_file>" << std::endl;
-        //return 0;
+        return 0;
     }
 
     NetworkSheduler *sheduler = new NetworkSheduler("config");
